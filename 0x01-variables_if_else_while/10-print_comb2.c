ec169e49ea558762be0c2bed93a99f45b7c962f4
daniela2001-png <dmbmora@outlook.com>
2020-02-06 23:23:43 +0000

    C

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 99 ; x++)
+		{
+			putchar ((x / 10) + '0');
+			putchar ((x % 10) + '0');
+
+			if (x != 99)
+				{
+
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	putchar('\n');
+	return (0);
+}
