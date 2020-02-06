8d0ef0c5205e564f3a7e8a6429b600c037f4cefe
daniela2001-png <dmbmora@outlook.com>
2020-02-06 19:16:12 +0000

    C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ srand(time(0));
 	/* your code goes there */
 	if ((n % 10) > 5)
 {
-		printf("Last digit of %d is %d and is greater than 5", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if ((n % 10) == 0)
 		{
