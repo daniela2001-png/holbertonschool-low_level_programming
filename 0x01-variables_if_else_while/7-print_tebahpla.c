255533dada8832c67a5ee6691aeb79b27d405c93
daniela2001-png <dmbmora@outlook.com>
2020-02-06 23:01:59 +0000

    C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'z' ; az >= 'a' ; az--)
+
+		putchar(az);
+	putchar('\n');
+	return (0);
+}
