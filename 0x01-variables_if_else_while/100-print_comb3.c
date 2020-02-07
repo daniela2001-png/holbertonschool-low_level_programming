ba37cfea8e1d05bd89f428cec5c5f92b29c99e48
daniela2001-png <dmbmora@outlook.com>
2020-02-07 18:57:51 +0000

    C

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,17 @@
  */
 int main(void)
 {
-
-	int  x;
-	int y;
-
-	for (x = '0' ; x <= '9' ; x++)
-		{
-			for (y = x + 1 ; y <= '9' ; y++)
-				{
-				  putchar (x);
-				  putchar (y);
-				  if (x != '8' || y != '9')
-					  putchar(',');
-				  putchar(' ');	  
-				}
-		}		
-				   putchar('\n');
-				
-	return (0);
+int x;
+int y;
+for (x = '0' ; x <= '9' ; x++)
+for (y = x + 1 ; y <= '9' ; y++)
+{
+putchar (x);
+putchar (y);
+if (x != '8' || y != '9')
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
