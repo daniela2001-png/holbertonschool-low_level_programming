3547cd32d9f307a5878a51bc59e99ee852f9a7a6
daniela2001-png <dmbmora@outlook.com>
2020-02-11 18:04:52 +0000

    holberton.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @x: The character to print
+ * Return: Always 0.
+ */
+int print_last_digit(int x)
+{
+
+	x = x % 10;
+	if (x < 0)
+		x = x * -1;
+			_putchar('0' + x);
+	return (x);
+}
