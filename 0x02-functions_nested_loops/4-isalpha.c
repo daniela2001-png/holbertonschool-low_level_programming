57e192ccc86aab496e221da53bf7fe7e8b3ce235
daniela2001-png <dmbmora@outlook.com>
2020-02-11 15:36:46 +0000

    holberton.h

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,18 +6,19 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		{ 
-			return (1);
-		}	
-
-	else if (c >= 'A' && c <= 'Z')
+		if (c >= 'a' && c <= 'z')
 		{
 			return (1);
 		}
 
-	else
+		else if (c >= 'A' && c <= 'Z')
 		{
-			return (0);
+
+			return (1);
+		}
+
+		else
+			{
+				return (0);
 		}
 }
