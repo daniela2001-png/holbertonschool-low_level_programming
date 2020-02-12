44cf8b9605d051222219e9de76ea61e78da2c243
daniela2001-png <58857379+daniela2001-png@users.noreply.github.com>
2020-02-12 00:14:12 -0500

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,59 +1,45 @@
 #include "holberton.h"
-
 /**
- * print_times_table - check the code for Holberton School students.
- * @n: The character to print
- * Return: Always 0.
- */
-
+*  print_times_table - check the code for Holberton School students.
+*@n:integer.
+* Return: Always.
+*/
 void print_times_table(int n)
 {
-int a, b, mult;
-
+        int a, b, mult;
 if (n < 15 && n >= 0)
 {
-for (a = 0; a <= n; a++)
-{
-for (b = 0; b <= n; b++)
-{
-mult = a * b;
-
-if (mult < 10)
-{
-if (b != 0)
-{
-putchar (',');
-putchar (' ');
-putchar (' ');
-putchar (' ');
-}
-putchar ((mult % 10) + '0');
-}
-else if (mult >= 10 && mult <= 99)
-{
-if (b != 0)
-{
-putchar (',');
-putchar (' ');
-putchar (' ');
-}
-putchar ((mult / 10) + '0');
-putchar ((mult % 10) + '0');
-}
-else if (mult >= 100)
-{
-if (b != 0)
-{
-putchar (',');
-putchar (' ');
-}
-putchar ((mult / 100) + '0');
-
-putchar (((mult / 10) % 10) + '0');
-putchar ((mult % 10) + '0');
-}
-}
-putchar ('\n');
-}
-}
+        for (a = 0; a <= n; a++)
+        {
+                for (b = 0; b <= n; b++)
+                {
+                        mult = a * b;
+                        if (mult < 10)
+                        {
+                                if (b != 0)
+                                {
+                                _putchar (',');
+                                _putchar (' ');
+                                _putchar (' ');
+                                _putchar (' '); }
+                        _putchar ((mult % 10) + '0');   }
+                        else if (mult >= 10 && mult <= 99)
+                        {
+                                if (b != 0)
+                                {
+                                _putchar (',');
+                                _putchar (' ');
+                                _putchar (' '); }
+                        _putchar ((mult / 10) + '0');
+                        _putchar ((mult % 10) + '0');   }
+                        else if (mult >= 100)
+                        {
+                                if (b != 0)
+                                {
+                                _putchar (',');
+                                _putchar (' '); }
+                        _putchar ((mult / 100) + '0');
+                        _putchar (((mult / 10) % 10) + '0');
+                        _putchar ((mult % 10) + '0');   }       }
+_putchar ('\n');        }       }
 }
