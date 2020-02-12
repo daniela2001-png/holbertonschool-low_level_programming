1933354edf1cf6acf4beacf40585d760bde7cdb2
daniela2001-png <58857379+daniela2001-png@users.noreply.github.com>
2020-02-12 00:07:02 -0500

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,45 +8,54 @@
 
 void print_times_table(int n)
 {
-    int a, b, mult;
+int a, b, mult;
 
 if (n < 15 && n >= 0)
 {
-    for (a = 0; a <= n; a++)
-    {
-        for (b = 0; b <= n; b++)
-        {
-            mult = a * b;
+for (a = 0; a <= n; a++)
+{
+for (b = 0; b <= n; b++)
+{
+mult = a * b;
 
-            if (mult < 10)
-            {
-                if (b != 0)
-                {
-                putchar (',');
-                putchar (' ');
-                putchar (' ');
-                putchar (' ');    }
-            putchar ((mult % 10) + '0');    }
-            else if (mult >= 10 && mult <= 99)
-            {
-                if (b != 0)                {
-                putchar (',');
-                putchar (' ');
-                putchar (' ');    }
-            putchar ((mult / 10) + '0');
-            putchar ((mult % 10) + '0');    }
-            else if (mult >= 100)
-            {
-                if (b != 0)
-                {
-                putchar (',');
-                putchar (' ');    }
-            putchar ((mult / 100) + '0');
+if (mult < 10)
+{
+if (b != 0)
+{
+putchar (',');
+putchar (' ');
+putchar (' ');
+putchar (' ');
+}
+putchar ((mult % 10) + '0');
+}
+else if (mult >= 10 && mult <= 99)
+{
+if (b != 0)
+{
+putchar (',');
+putchar (' ');
+putchar (' ');
+}
+putchar ((mult / 10) + '0');
+putchar ((mult % 10) + '0');
+}
+else if (mult >= 100)
+{
+if (b != 0)
+{
+putchar (',');
+putchar (' ');
+}
+putchar ((mult / 100) + '0');
 
-            putchar (((mult / 10) % 10) + '0');
-            putchar ((mult % 10) + '0');    }    }
-putchar ('\n');    }    }
-
+putchar (((mult / 10) % 10) + '0');
+putchar ((mult % 10) + '0');
+}
+}
+putchar ('\n');
+}
+}
 }
 
 
