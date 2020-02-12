200eae47935a0d25128aefc4149c84bd38c9e480
daniela2001-png <58857379+daniela2001-png@users.noreply.github.com>
2020-02-11 23:04:02 -0500

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
+
 /**
- * add- check the code for Holberton School students.
+ * add - check the code for Holberton School students.
  * @a: The character to print
  * @b: The character to print
  *Return: Always 0.
  */
+
 int add(int a, int b)
 {
     int suma = a + b;
-
-    return (suma);
+        return (suma);
 }
