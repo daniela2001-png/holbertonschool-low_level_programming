a3d5f81e48e593b5673e4fe355af77e50cab04be
daniela2001-png <58857379+daniela2001-png@users.noreply.github.com>
2020-02-11 23:34:04 -0500

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,27 @@
+#include "holberton.h"
 
+/**
+ * print_times_table - check the code for Holberton School students.
+ * @: The character to print
+ * Return: Always 0.
+ */
+
+void print_times_table(int n)
+{
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
