c0d71cd01273d05f7a7537a9af921b9e5c57232a
daniela2001-png <58857379+daniela2001-png@users.noreply.github.com>
2020-02-11 23:13:05 -0500

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,35 @@
 #include "holberton.h"
+#include <stdio.h>
+/**
+* print_to_98 - prints al natural numbers to 98
+* @n: The character to print
+*
+*/
+
+void print_to_98(int n)
+{
+	int a;
+
+	if (n < 98)
+	{
+		for (a = n; a < 98; a++)
+		{
+		printf("%d, ", a);
+		}
+		printf("98");
+		putchar('\n');
+	}
+	if (n > 98)
+	{
+		for (a = n; a > 98; a--)
+		{
+		printf("%d, ", a);
+		}
+		printf("98\n");
+	}
+	if (n == 98)
+	{
+		printf("98\n");
+	}
+}
+
