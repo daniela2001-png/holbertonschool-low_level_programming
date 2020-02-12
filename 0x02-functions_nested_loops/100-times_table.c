e15d09fd393201c984dc97e278bb690f07fb6f3c
daniela2001-png <58857379+daniela2001-png@users.noreply.github.com>
2020-02-11 23:55:37 -0500

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,19 +2,52 @@
 
 /**
  * print_times_table - check the code for Holberton School students.
- * @: The character to print
+ * @n: The character to print
  * Return: Always 0.
  */
 
 void print_times_table(int n)
 {
-  
+    int a, b, mult;
 
+if (n < 15 && n >= 0)
+{
+    for (a = 0; a <= n; a++)
+    {
+        for (b = 0; b <= n; b++)
+        {
+            mult = a * b;
 
+            if (mult < 10)
+            {
+                if (b != 0)
+                {
+                putchar (',');
+                putchar (' ');
+                putchar (' ');
+                putchar (' ');    }
+            putchar ((mult % 10) + '0');    }
+            else if (mult >= 10 && mult <= 99)
+            {
+                if (b != 0)                {
+                putchar (',');
+                putchar (' ');
+                putchar (' ');    }
+            putchar ((mult / 10) + '0');
+            putchar ((mult % 10) + '0');    }
+            else if (mult >= 100)
+            {
+                if (b != 0)
+                {
+                putchar (',');
+                putchar (' ');    }
+            putchar ((mult / 100) + '0');
 
+            putchar (((mult / 10) % 10) + '0');
+            putchar ((mult % 10) + '0');    }    }
+putchar ('\n');    }    }
 
-
-
+}
 
 
 
