dacf84a915b06dca64a88b8f1e820e5f93204b76
daniela2001-png <dmbmora@outlook.com>
2020-02-13 23:15:20 +0000

    functions

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square - check the code for Holberton School students.
+ * @size : type size square
+ *
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		{
+			_putchar('\n');
+		}
+
+	for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+				{
+					_putchar('#');
+				}
+
+			_putchar('\n');
+		}
+
+}
