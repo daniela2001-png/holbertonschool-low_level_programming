8c7c4286a807848c083fee888bb684288dd76e78
daniela2001-png <dmbmora@outlook.com>
2020-02-13 23:21:54 +0000

    functions

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,18 +10,15 @@ void print_square(int size)
 	int a, b;
 
 	if (size <= 0)
-		{
-			_putchar('\n');
-		}
-
+	{
+		_putchar('\n');
+	}
 	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
 		{
-			for (b = 1; b <= size; b++)
-				{
-					_putchar('#');
-				}
-
-			_putchar('\n');
+			_putchar('#');
 		}
-
+	_putchar('\n');
+	}
 }
