254af5716497c6935deb3523f44481c567fe16f1
daniela2001-png <dmbmora@outlook.com>
2020-02-13 20:13:59 +0000

    functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n: number of lines
+ * 
+ */
+
+void print_line(int n)
+{
+	int y;
+
+	for (y = 0 ; y <= n ; y++)
+		{
+			if (n > 0)
+				{
+					_putchar('_');
+				}
+
+
+		}
+			_putchar('\n');
+
+
+}
