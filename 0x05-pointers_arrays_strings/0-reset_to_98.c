6d55cad5d4387a8d92e293f6a4cc650e84a8b048
daniela2001-png <dmbmora@outlook.com>
2020-02-17 22:05:00 +0000

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,14 @@
 
 /**
  * reset_to_98 - check the code for Holberton School students.
- *
+ * @n : pointer int
  * Return: Always 0.
  */
 
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 
-*n
+	*n = 98;
+
 }
