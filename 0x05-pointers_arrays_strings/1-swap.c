fd66b0e19c361aefdef67e620aa837a634900535
daniela2001-png <dmbmora@outlook.com>
2020-02-17 22:26:41 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a : pointer
+ * @b : pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+int z;
+
+z = *a;
+
+*a = *b;
+
+*b = z;
+
+}
