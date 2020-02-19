ff8d9329141f54a6308f0a9c665466a374005f04
daniela2001-png <dmbmora@outlook.com>
2020-02-19 02:05:27 +0000

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,47 +8,27 @@
 
 void puts_half(char *str)
 {
-
 int tam = 0;
 int i;
 
 while (str[tam] != '\0')
-
 {
-
 tam++;
-
 }
-
 if (tam % 2 != 0)
-
 {
-
 for (i = ((tam + 1) / 2); i < tam; i++)
-
 {
-
 _putchar(str[i]);
-
 }
-
 }
-
 else
-
 {
-
 for (i = (tam / 2); i < tam; i++)
-
 {
-
 _putchar(str[i]);
-
 }
-
 }
-
 _putchar('\n');
-
 }
 
