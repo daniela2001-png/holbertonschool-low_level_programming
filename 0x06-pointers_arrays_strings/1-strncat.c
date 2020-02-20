8eb7da8c6b8635e8f01440329705e0f1f28bd8f1
daniela2001-png <dmbmora@outlook.com>
2020-02-20 17:55:21 +0000

    pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,24 +4,24 @@
  * _strncat - check the code for Holberton School students.
  * @dest : pointer
  * @src : pointer
- * @n : integer 
+ * @n : integer
  * Return: Always 0.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 
-	int d =0;
-	int s =0;
+	int d = 0;
+	int s = 0;
 
-	while (dest[d] != '\0')
+		while (dest[d] != '\0')
 		{
 			d++;
 
 		}
 
-	while(src[s] != '\0' && s < n)
-		{ 
+		while (src[s] != '\0' && s < n)
+		{
 			dest[d] = src[s];
 				d++;
 				s++;
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	int i = 0;
 
-		while ( src[i] != '\0' && i < n)
+		while (src[i] != '\0' && i < n)
 		{
 			dest[i] = src[i];
 			i++;
@@ -23,7 +23,7 @@ char *_strncpy(char *dest, char *src, int n)
 		while (i < n)
 		{
 
-			dest[i] = '\0';
+			xdest[i] = '\0';
 			i++;
 		}
 
