14e2a2da45b3edf4748a807979fb7956087c557b
daniela2001-png <dmbmora@outlook.com>
2020-02-20 22:55:52 +0000

    pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@ void reverse_array(int *a, int n)
 	int tmp;
 	int i;
 
-	for (i = 0; i < (n / 2); i++)
+		for (i = 0; i < (n / 2); i++)
 		{
 			tmp = a[i];
-			a[i] = a[n - i -1];
+			a[i] = a[n - i - 1];
 			a[n - i - 1] = tmp;
 
 
