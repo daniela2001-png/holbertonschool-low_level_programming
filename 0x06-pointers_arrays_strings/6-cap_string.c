f8a20a9e5525b40357eb38de638c01557954c470
daniela2001-png <dmbmora@outlook.com>
2020-02-21 02:54:58 +0000

    [Feature] Function to cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,22 @@
- #include "holberton.h"
-
+#include "holberton.h"
 /**
  * *cap_string - check the code for Holberton School students.
- * @ : pointer
+ * @s : pointer
  * Return: Always 0.
  */
-
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	int i = 0;
 
-
-
-
-
-
-
-
-
+	while (s[i] != '\0')
+	{
+		if ((s[i - 1] == 32 || s[i - 1] == 10 || s[i - 1] == 9
+			|| s[i - 1] == 46 || (s[0] >= 'a' && s[0] <= 'z'))
+			&& (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
 }
