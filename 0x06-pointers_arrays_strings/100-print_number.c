82a4fe857b94daa30091aa3340a25fca92a6ef21
daniela2001-png <dmbmora@outlook.com>
2020-02-22 02:33:41 +0000

    pointers

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_number - check the code for Holberton School students.
+ * @n : integer
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	if (n > 9)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
