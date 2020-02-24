b4009085cf134ca1b72951c6650f02af57c23c22
daniela2001-png <dmbmora@outlook.com>
2020-02-24 19:30:57 +0000

    pointers7

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *_strpbrk - check the code for Holberton School students.
+ * @s : the string pointed
+ * @accept : the charcter to found
+ * Return: s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+			while (*s != '\0')
+			{
+				for (j = 0; accept[j] != '\0'; j++)
+				{
+					if (s[i] == accept[j])
+					{
+						return (s);
+					}
+				}
+			s++;
+			}
+	return (0);
+}
