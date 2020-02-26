3cc66afcd28466650b499309ebd6365723e90b69
daniela2001-png <dmbmora@outlook.com>
2020-02-26 17:11:20 +0000

    recursion is fun

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - check the code for Holberton School students.
+ * @s : is a pointer
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s != '\0')
+		{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+		}
+		else if (*s == '\0')
+		{
+			_putchar('\n');
+		}
+}
