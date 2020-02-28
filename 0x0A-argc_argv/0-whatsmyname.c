058d9f960552cdaa3fb29ebe4cc32075868ad6e5
daniela2001-png <dmbmora@outlook.com>
2020-02-28 17:43:48 +0000

    static library

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc : int
+ * @argv : string
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+
+
+}
