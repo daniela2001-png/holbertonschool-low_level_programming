9e29d4f3d880bf60e36cf1f8b4d57aff6c4b3ee3
daniela2001-png <dmbmora@outlook.com>
2020-03-04 01:56:41 +0000

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,13 +1,6 @@
 #include <stdlib.h>
 #include "holberton.h"
 
-/**
- * alloc_grid - check the code for Holberton School students.
- * @width: na
- * @height: no
- * Return:pointer of array
- */
-
 int **alloc_grid(int width, int height)
 {
 
@@ -17,21 +10,20 @@ int col;
 int filas;
 
 if (width <= 0 || height <= 0)
-{
 return (NULL);
 
-}
-
 p = malloc(sizeof(int *) * height);
+
+if (p == NULL)
+return (NULL);
+
 for (filas = 0; filas < height; filas++)
 {
 p[filas] = malloc(sizeof(int) * width);
-
 if (p[filas] == NULL)
 {
 for (x = 0; x < filas; x++)
 free(p[filas]);
-
 free(p);
 return (NULL);
 }
@@ -40,11 +32,7 @@ return (NULL);
 for (filas = 0; filas < height; filas++)
 {
 for (col = 0; col < width; col++)
-{
 p[filas][col] = 0;
-}
-
-}
-return (p);
-
+}       
+return (p);   
 }
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void free_grid(int **grid, int height)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
