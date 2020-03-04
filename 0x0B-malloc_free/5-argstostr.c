fa49fbbe7bb1b734483c8ed5f1c344df2f54857b
daniela2001-png <dmbmora@outlook.com>
2020-03-04 03:39:43 +0000

    malloc

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - check the code for Holberton School students.
+ * @ac : as
+ * @av : ad
+ * Return: Always 0.
+ */
+char *argstostr(int ac, char **av)
+{
+
+char *s;
+int i, j, c = 0;
+int count = 0;
+
+if (ac == 0 || av == 0)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+count++;
+}
+}
+s = (char *)malloc((ac + count + 1) * sizeof(char));
+if (s == NULL)
+{
+free(s);
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+s[c] = av[i][j];
+c++;
+}
+s[c] = '\n';
+c++;
+}
+return(s);
+}
