5bf26ed911338cf3d5f8f60670ff3fd3f6551ee1
daniela2001-png <dmbmora@outlook.com>
2020-03-04 03:00:14 +0000

    malloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,25 +2,19 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * free_grid - check the code for Holberton School students.
+ * @grid: ns
+ * @height: nj
  * Return: Always 0.
  */
 
 void free_grid(int **grid, int height)
 {
-
-
-
-
-
-
-
-
-
-
-
-
-
+int x;
+for (x = 0; x < height; x++)
+{
+free(grid[x]);
+}
+free(grid);
 
 }
