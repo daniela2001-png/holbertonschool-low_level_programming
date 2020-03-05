bedf2bdb384af078364dbcb529de5fa5b0d32b35
daniela2001-png <dmbmora@outlook.com>
2020-03-05 23:29:12 +0000

    more malloc and free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,29 @@
 #include <stdlib.h>
 #include "holberton.h"
 
+/**
+ * malloc_checked - check the code for Holberton School students.
+ * @b : int
+ * Return: pointer
+ */
 
+
+
+
+
+void *malloc_checked(unsigned int b)
+{
+
+void *d;
+
+d = malloc(b);
+
+if (d == NULL)
+{
+
+exit(98);
+
+}
+return (d);
+
+}
