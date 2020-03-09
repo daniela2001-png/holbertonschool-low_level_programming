7005d71f7066da743954d68bbf23c95596dad34d
daniela2001-png <dmbmora@outlook.com>
2020-03-09 18:46:32 +0000

    structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,10 @@
-#ifndef dog_h
-#define dog_h
-
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - check the code for Holberton School students.
+ *struct dog - check the code for Holberton School students.
  *@name : name
  *@age : age
  *@owner : owner
- * Return: Always 0.
  */
 
 struct dog
@@ -18,25 +16,8 @@ char *owner;
 
 
 };
-
-/**
- * struct dog_da - check the code for Holberton School students.
- *@name : name
- *@age : age
- *@owner : owner
- *Return: Always 0.
- */
-typedef struct dog_da
-{
-
-char *name;
-float age;
-char *owner;
-} dog_da;
-
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_da *d);
 #endif
