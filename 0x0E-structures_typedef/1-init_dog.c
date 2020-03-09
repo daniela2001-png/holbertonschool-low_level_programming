86e99f33af2441b631a29132d59c7e3d83fe3772
daniela2001-png <dmbmora@outlook.com>
2020-03-09 20:13:31 +0000

    structs

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-struct dog;
-
-(*d).name = name;
-(*d).owner = owner;
-(*d).age = age;
+if (d != NULL)
+{
+d->name = name;
+d->owner = owner;
+d->age = age;
+}
 }
