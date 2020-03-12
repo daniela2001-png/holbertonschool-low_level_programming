219887d7bf12c41248706aba18cf920cacbe473c
daniela2001-png <dmbmora@outlook.com>
2020-03-12 02:02:33 +0000

    task calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * op_add - function to add
+ * @b: them num2
+ * @a: the num1
+ * Return: the add between a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function to sub
+ * @a:num1
+ * @b:num2
+ * Return: the sub the a & b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function to mul
+ * @a: num1
+ * @b: num2
+ * Return: the mul the a & b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function to div
+ * @a: num1
+ * @b: num2
+ * Return: the div between a & b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function to mod
+ * @a: num1
+ * @b: num2
+ * Return: the mod between a & b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
