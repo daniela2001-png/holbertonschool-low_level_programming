7a7c3c632f92178bf79224bc3fb58639559ed9f0
daniela2001-png <dmbmora@outlook.com>
2020-03-12 02:40:02 +0000

    task calculator

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,12 @@
-#ifndef calc_h
-#define calc_h
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
