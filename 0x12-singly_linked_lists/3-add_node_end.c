0019bd00c2d00af57059b1d8cb20a5b6562a08af
daniela2001-png <dmbmora@outlook.com>
2020-03-20 00:18:15 -0500

    jumping between nodes

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		tmp = *head;
 		while (tmp->next != NULL)
 			tmp = tmp->next;
-		temp->next = newnode;
+		tmp->next = newnode;
 	}
 
 	return (*head);
