95a6891eeef2e7d7382795b62fce97ac80969fab
daniela2001-png <dmbmora@outlook.com>
2020-03-20 00:15:57 -0500

    jumping between nodes

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_node_end - created a tail node
+ * @head: pointer to my list link
+ * @str: pointer to string in main
+ * Return: pointer head
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+	list_t *newnode, *tmp;
+	unsigned int size = 0;
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->str = strdup(str);
+	while (str[size])
+	{
+		size++;
+	}
+	newnode->len = size;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		temp->next = newnode;
+	}
+
+	return (*head);
+}
