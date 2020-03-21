3ee612e7a2195f67cd4d4b93eb9f31250c619506
daniela2001-png <dmbmora@outlook.com>
2020-03-21 13:32:04 -0500

    jumping between nodes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * premain - function that executed before of my function main
+ * Return: nothing
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
