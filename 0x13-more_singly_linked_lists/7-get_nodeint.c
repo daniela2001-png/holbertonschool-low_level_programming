d4bbe16d559716e1b2b7d5b7d1265c9a7b9841de
daniela2001-png <dmbmora@outlook.com>
2020-03-23 17:10:48 -0500

    jumping between more nodes

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - check the code for Holberton School students.
+ * @head:pointer to list
+ * @index: integer than recurrent my list
+ * Return: index n
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *tmp = head;
+
+	while (tmp != NULL)
+	{
+		if (head == 0)
+			return (0);
+		count++;
+		tmp = tmp->next;
+
+		if (count == index)
+			break;
+	}
+	return (tmp);
+}
