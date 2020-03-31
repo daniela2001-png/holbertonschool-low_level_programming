496d070097703b0188a04a7aebb4e220b6b7eccd
daniela2001-png <dmbmora@outlook.com>
2020-03-31 15:49:11 -0500

    file management

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ *  create_file - check the code for Holberton School students.
+ * @filename: file to create
+ * @text_content:string to write
+ * Return: 1 itext_contentn case of exit
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int pf;
+	int i = 0;
+
+	if (!filename)
+		return (-1);
+
+	pf = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (pf == -1)
+		return (-1);
+
+		while (text_content[i])
+			i++;
+		if (text_content == NULL)
+		{
+			close(pf);
+			return (-1);
+		}
+		else
+			write(pf, text_content, i);
+
+		close(pf);
+		return (1);
+}
