c8bb87ff55e3d8272e6cc21837514868ae019659
usuario <dmbmora@outlook.com>
2020-05-02 15:40:30 -0500

    double lists in C

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,6 +9,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
+	dlistint_t *last;
 
 	newnode = malloc(sizeof(dlistint_t));
 
@@ -17,7 +18,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		free(newnode);
 		return (NULL);
 	}
-	dlistint_t *last = *head;
 
 	newnode->n = n; /* Put the date int n */
 	newnode->next = NULL; /* This new node is going to be the last node, so */
@@ -26,17 +26,18 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		newnode->prev = NULL;
 		*head = newnode;
+		return (newnode);
 	}
 	else
 	{
+		last = *head;
 		while (last->next != NULL)
 			last = last->next;
 		/* Change the next of last node */
 		last->next = newnode;
 		/* Make last node as previous of new node */
 		newnode->prev = last;
-		last = newnode;
 	}
 	/* the address of the new element */
-	return (*head);
+	return (newnode);
 }
