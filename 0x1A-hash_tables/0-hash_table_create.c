e1005a5c0600fa936e14f1bb2a9e7e6f31639974
daniela2001-png <dmbmora@outlook.com>
2020-05-28 17:59:22 -0500

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,21 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - function hash 
+ * hash_table_create - function hash
  * @size:size of a array
  * Return:a pointer to a new hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *table;
-        hash_node_t *node;
-        
-        table = malloc(sizeof(*table));
-        if (table == NULL)
-                return (NULL);
-        table->size = size;
-        table->array = malloc(sizeof(node) * size);
-        if (table->array == NULL)
-                return (NULL);
-        return (table);
+		hash_table_t *table;
+		hash_node_t *node;
+
+		table = malloc(sizeof(*table));
+		if (table == NULL)
+			return (NULL);
+		table->size = size;
+		table->array = malloc(sizeof(node) * size);
+		if (table->array == NULL)
+			return (NULL);
+		return (table);
 }
