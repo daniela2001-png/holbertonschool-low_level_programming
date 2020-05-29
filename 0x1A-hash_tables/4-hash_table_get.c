47af44eacee2bdf3589c8b7a62678515f4867bef
daniela2001-png <dmbmora@outlook.com>
2020-05-29 10:08:19 -0500

    hash

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that found a key from the value
+ * @ht:hash table
+ * @key:keyword
+ * Return: the value associated with the element
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *ptr;
+	unsigned long int index;
+
+	if (key == NULL || ht->array == NULL || ht->size == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[index];
+	while (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+			return (ptr->value);
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
