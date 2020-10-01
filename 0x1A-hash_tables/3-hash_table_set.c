b811de47f9a61e493e4f880698f7139b995feebc
daniela2001-png <dmbmora@outlook.com>
2020-10-01 16:22:37 -0500

    adding point 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,49 +1,40 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - function that adds a key/value to ht
- * @ht:hash table to update o adds
- * @key:keyword
- * @value:value to key
- * Return:1 if it succeeded, 0 otherwise
+ * hash_table_set - function that add element to ht
+ * @key:key value
+ * @ht:is the hash table
+ * @value: value of the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node_added;
+	hash_node_t *nodo = NULL;
 
-	if (key == NULL || ht == NULL || value == NULL)
+	if (!key)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index])
+	{
 		if (strcmp(ht->array[index]->key, key) == 0)
 		{
-			free(ht->array[index]->value); /*free the value to change*/
-			/*for the new value*/
-			ht->array[index]->value = strdup(value);/*Asignamos el*/
-			/*nuevo valor de la akey*/
-			if (!ht->array[index]->value)
-				return (0);
+			free(ht->array[index]->value);
+			ht->array[index]->value = (char *)value;
 			return (1);
 		}
-	node_added = malloc(sizeof(hash_node_t));
-	if (!node_added)
-		return (0);
-	node_added->key = strdup(key); /*I done a strdup cuz*/
-	/*the key(left) do not have space*/
-	node_added->value = strdup(value); /*I done a strdup cuz*/
-	/*the value(left) do not have space*/
-
-	if (node_added->key == NULL || node_added->value == NULL)
-	{
-		free(node_added->value);
-		free(node_added->key);
-		return (0);
 	}
-	node_added->next = ht->array[index]; /*apuntamos donde vamos*/
-	/*a colocar el nodo*/
-	ht->array[index] = node_added;/*colocamos el newnode*/
 
+	nodo = malloc(sizeof(hash_node_t));
+	if (!nodo)
+		return (0);
+	nodo->key = (char *)(key);
+	nodo->value = (char *)(value);
+	nodo->next = ht->array[index];
+	ht->array[index] = nodo;
 	return (1);
+
 
 }
