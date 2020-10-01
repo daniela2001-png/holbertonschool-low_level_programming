26f16134dde879776596fd31c0724d08f7e55bcf
daniela2001-png <dmbmora@outlook.com>
2020-10-01 18:00:34 -0500

    adding point 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,13 +1,15 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_get - function that found a key from the value
- * @ht:hash table
- * @key:keyword
- * Return: the value associated with the element
+ * hash_table_get - return the value from the key
+ * @ht: is the hash table
+ * @key: is the key of the array
+ * Return: Returns the value associated with the element, or NULL if key couldn’t be found 
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *ptr;
+	//hash_node_t *ptr;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || ht->array == NULL ||
@@ -15,12 +17,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	ptr = ht->array[index];
-	while (ptr != NULL)
+	// ptr = ht->array[index];
+	if (ht->array[index])
 	{
-		if (strcmp(ptr->key, key) == 0)
-			return (ptr->value);
-		ptr = ptr->next;
+		if (strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
 	}
 	return (NULL);
+
 }
