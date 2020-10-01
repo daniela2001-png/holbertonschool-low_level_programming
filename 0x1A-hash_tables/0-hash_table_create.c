29ba5744ddd6b2d54680240de181250e381b23d2
daniela2001-png <dmbmora@outlook.com>
2020-10-01 13:33:06 -0500

    adding file point zero and README.md

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,21 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - function hash
- * @size:size of a array
- * Return:a pointer to a new hash table
+ * hash_table_create - function that creates a hash table
+ * @size: sizeof the table
+ * Return: a pointer to a hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-		hash_table_t *table;
-		hash_node_t *node;
+	/* 1ro creo un puntero de tipo struct hash_table_t */
+	/* que sera el que retornare */
+	hash_table_t *table = NULL;
+	hash_node_t *size_node = NULL; /* puntero = 8 bytes */
 
-		table = malloc(sizeof(*table));
-		if (table == NULL)
-			return (NULL);
-		table->size = size;
-		table->array = malloc(sizeof(node) * size);
-		if (table->array == NULL)
-			return (NULL);
-		return (table);
+
+	table = malloc(sizeof(*table)); /* aqui asigno los 16 bytes*/
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(size_node) * size); /* 8 * 1024 bytes */
+	/* si el malloc falla retorno NULL*/
+	if (table->array == NULL)
+		return (NULL);
+	/* return the pointer */
+	return (table);
 }
