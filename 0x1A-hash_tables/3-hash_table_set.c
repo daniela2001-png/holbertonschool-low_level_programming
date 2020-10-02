2804586dea8bbee37b69adbaab436d1aa46a6be9
daniela2001-png <dmbmora@outlook.com>
2020-10-02 16:22:49 -0500

    adding point 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *nodo = NULL;
 
-	if (!key)
+	if (!key || !ht)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -34,7 +34,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	nodo->key = strdup(key);
 	nodo->value = strdup(value);
-	
 	if (!nodo->key || !nodo->value)
 	{
 		free(nodo->value);
