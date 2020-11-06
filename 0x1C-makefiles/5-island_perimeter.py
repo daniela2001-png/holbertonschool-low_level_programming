7086c9d9988788fa62f62030ff3edc53e23b75fd
daniela2001-png <dmbmora@outlook.com>
2020-11-06 14:40:01 -0500

    [new]  island_perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,7 +9,10 @@ def island_perimeter(grid):
     retornar el perimetro de la isla
     """
     lista = [vector for scalar in grid for vector in scalar if vector == 1]
-    suma = 0
-    for uno in lista:
-        suma += uno
-    return ((suma * 2) + 2)
+    if lista:
+        suma = 0
+        for uno in lista:
+            suma += uno
+        return ((suma * 2) + 2)
+    else:
+        return 0
