65b1baa865e8f87c29a0a77218addb9931115c7b
daniela2001-png <dmbmora@outlook.com>
2020-11-06 19:39:33 -0500

    [new]  island_perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 """
-matriz de 3x3
+matriz
 """
 
 
@@ -8,11 +9,18 @@ def island_perimeter(grid):
     """
     retornar el perimetro de la isla
     """
-    lista = [vector for scalar in grid for vector in scalar if vector == 1]
-    if lista:
-        suma = 0
-        for uno in lista:
-            suma += uno
-        return ((suma * 2) + 2)
-    else:
-        return 0
+    perimeter = 0
+    for columna in range(len(grid)):
+        for fila in range(len(grid[columna])):
+            if grid[columna][fila] == 1:
+                if columna - 1 < 0 or grid[columna - 1][fila] == 0:
+                    perimeter += 1
+                # aqui lo valida con el len de la matriz ya que este
+                # es elmismo numero de cols
+                if columna + 1 >= len(grid) or grid[columna + 1][fila] == 0:
+                    perimeter += 1
+                if fila + 1 >= len(grid[0]) or grid[columna][fila + 1] == 0:
+                    perimeter += 1
+                if fila - 1 < 0 or grid[columna][fila - 1] == 0:
+                    perimeter += 1
+    return perimeter
