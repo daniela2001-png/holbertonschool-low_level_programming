26264c62742b43e2636707627dfb472158657c1a
daniela2001-png <dmbmora@outlook.com>
2021-01-13 14:34:41 -0500

    [Add] adding point zero

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search an elemetn insie of a array and return ur index
+ * @array: the array with n elements to verificate
+ * @size: the size of the array to over
+ * @value: the element to search
+ * Return: a integer that is the index of the value found inside of a array!
+ **/
+
+int linear_search(int *array, size_t size, int value)
+{
+
+	int i = 0;
+
+	if (array == NULL)
+		return (-1);
+	while ((size_t)i < size)
+	{
+		printf("Value checked array %d = %d\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
