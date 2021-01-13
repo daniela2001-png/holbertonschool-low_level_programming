96468f1ed897b94e859bffdde7f70b898ff4d62a
daniela2001-png <dmbmora@outlook.com>
2021-01-13 15:03:59 -0500

    [Add] adding folder main

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,13 +10,13 @@
 int linear_search(int *array, size_t size, int value)
 {
 
-	int i = 0;
+	size_t i = 0;
 
-	if (array == NULL || size == 0)
+	if (!array || size == 0)
 		return (-1);
-	while ((size_t)i < size)
+	while (i < size)
 	{
-		printf("Value checked array %d = %d\n", i, array[i]);
+		printf("Value checked array %zu = %d\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 		i++;
